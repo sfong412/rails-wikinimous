ce1c3e8ba80be8d3b9e15647cb92dc0f75981e0d
Sherman Fong <sfong.ima@gmail.com>
2022-04-09 15:37:40 -0400

    barebones version done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get    "articles",          to: "articles#index", as: :articles
+
+  get    "articles/new",      to: "articles#new",  as: :new_article
+  post   "articles",          to: "articles#create"
+
+  # NB: The `show` route needs to be *after* `new` route.
+  get    "articles/:id",      to: "articles#show", as: :article
+
+  get    "articles/:id/edit", to: "articles#edit", as: :edit_article
+  patch  "articles/:id",      to: "articles#update"
+
+  delete "articles/:id",      to: "articles#destroy"
 end
